9479ceb05651efb663ba52da45c7e7ed4c52a8d1
WeiSheng <842469165@qq.com>
2024-03-26 20:19:45 +0800

    update:测试git

--- MethodsTest/src/com/xiaoguo/EqualsReset/StaticTest.java
+++ MethodsTest/src/com/xiaoguo/EqualsReset/StaticTest.java
@@ -9,4 +9,44 @@ package com.xiaoguo.EqualsReset;
  * Description:
  */
 public class StaticTest {
+    public static void main(String[] args) {
+        Chinese c1 = new Chinese();
+        c1.name = "姚明";
+        c1.age = 12;
+        Chinese c2 = new Chinese();
+        c2.name = "张伟";
+        c2.age = 33;
+
+
+        System.out.println(c1.toString());
+        System.out.println(c2.toString());
+        // System.out.println();
+        c2.setNation("CNN");
+
+    }
+
+
 }
+
+class Chinese {
+    // 非静态变量
+    static String nation = "中国";
+
+    String name;
+    int age;
+    // 静态变量
+
+
+    public void setNation(String nation) {
+        Chinese.nation = nation;
+    }
+
+    @Override
+    public String toString() {
+        return "Chinese{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
