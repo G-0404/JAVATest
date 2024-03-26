33f034104132df657fb29fc35f922a63c82f38b9
郭某 <15824885026@163.com>
2024-03-26 20:10:50 +0800

    新建文件

---
+++ Zjt_itheima/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * @Author ZJT
+ * @Create 2024/3/26 16:29
+ * @Version 1.0
+ * ClassName: ${NAME}
+ * Package:
+ * Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
