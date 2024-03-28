8826db3524427cb4ccf34261e39af03b31a3a554
WeiSheng <842469165@qq.com>
2024-03-28 19:31:56 +0800

    update:更新jdk5.0之前的枚举类型定义

---
+++ innerClass/src/com/G04/innerClassTest/enumTest.java
@@ -0,0 +1,12 @@
+package com.G04.innerClassTest;
+
+/**
+ * @Author G04
+ * @Create 2024/3/28 11:28
+ * @Version 1.0
+ * ClassName: enumTest
+ * Package: com.G04.innerClassTest
+ * Description:
+ */
+public class enumTest {
+}
