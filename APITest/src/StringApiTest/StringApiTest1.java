79c6642a148d9bb7b447faed271017b5678e53fa
WeiSheng <842469165@qq.com>
2024-04-04 20:29:21 +0800

    update:1

---
+++ APITest/src/StringApiTest/StringApiTest1.java
@@ -0,0 +1,22 @@
+package StringApiTest;
+
+import org.junit.Test;
+
+/**
+ * @Author G04
+ * @Create 2024/4/4 15:30
+ * @Version 1.0
+ * ClassName: StroingApiTest1
+ * Package: StringApiTest
+ * Description:
+ */
+public class StringApiTest1 {
+    @Test
+    public void StringDemo() {
+        String str = "HEllo";
+
+        System.out.println(str);
+
+    }
+
+}
